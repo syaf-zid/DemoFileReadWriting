b0c7d4c3d1101b4d0fe96b18653d246ab7bfaf81
syafzid <18052270@myrp.edu.sg>
2020-07-17 20:46:54 +0800

    Completed with Worksheet Section D

--- app/src/main/java/com/myapplicationdev/android/demofilereadwriting/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demofilereadwriting/MainActivity.java
@@ -9,6 +9,7 @@ import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +23,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    String folderLocation;
     Button btnWrite, btnRead;
     TextView textView;
 
@@ -42,55 +44,64 @@ public class MainActivity extends AppCompatActivity {
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         }
 
-        // * Start of SECTION C *//
-        // Data Persistence -- Internal Storage
-        // String folderLocation_I = getFilesDir().getAbsolutePath() + "/Folder";
+        folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
 
-        // File folder_I = new File(folderLocation_I);
-        // if(!folder_I.exists()) {
-            // boolean result = folder_I.mkdir();
-            // if(result) {
-                // Log.d("File Read/Write", "Folder created");
-            // }
-        // }
+        File folder = new File(folderLocation);
+        if(!folder.exists()) {
+            boolean result = folder.mkdir();
 
-        try {
-            String folderLocation_I = getFilesDir().getAbsolutePath() + "/Folder";
-            File targetFile_I = new File(folderLocation_I, "test.txt");
-            FileWriter writer_I = new FileWriter(targetFile_I, true);
-            writer_I.write("test data" + "\n");
-            writer_I.flush();
-            writer_I.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Data Persistence -- File Reading
-        String folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Test";
-        File targetFile = new File(folderLocation, "data.txt");
-
-        if(targetFile.exists()) {
-            String data = "";
-            try {
-                FileReader reader = new FileReader(targetFile);
-                BufferedReader br = new BufferedReader(reader);
-
-                String line = br.readLine();
-                while(line != null) {
-                    data += line + "\n";
-                    line = br.readLine();
-                }
-
-                br.close();
-                reader.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                Toast.makeText(MainActivity.this, "Failed to read!", Toast.LENGTH_LONG).show();
-                e.printStackTrace();
+            if(result) {
+                Log.d("File Read/Write", "Folder created");
             }
-
-            Log.d("Content", data);
         }
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    File targetFile = new File(folderLocation, "section_d.txt");
+                    FileWriter writer = new FileWriter(targetFile, true);
+                    writer.write("Hello World!" + "\n");
+                    writer.flush();
+                    writer.close();
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this, "Failed to write!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File targetFile = new File(folderLocation, "section_d.txt");
+
+                if(targetFile.exists()) {
+                    String data = "";
+                    try {
+                        FileReader reader = new FileReader(targetFile);
+                        BufferedReader br = new BufferedReader(reader);
+
+                        String line = br.readLine();
+                        while(line != null) {
+                            data += line + "\n";
+                            line = br.readLine();
+                        }
+
+                        br.close();
+                        reader.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        Toast.makeText(MainActivity.this, "Failed to read!", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+
+                    Log.d("Content", data);
+                    textView.setText(data);
+                }
+            }
+        });
+
     }
 }
